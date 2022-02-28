576d74465610a4f6b94be392672a755a850eac84
Diego Ávila Maturana <94457305+AvilaMaturana@users.noreply.github.com>
2022-02-28 15:57:52 -0300

    avance falta agregar lo ultimo

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
+$(document).ready(function () {
+
+  window.addEventListener('resize', function(){
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('navbar-light' , window.innerWidth<1000);
+});  
+
+window.addEventListener('resize', function(){
+  var nav = document.getElementById('#navbarDos');
+  nav.classList.toggle('navbar-light' , window.innerWidth<1000);
+}); 
+
+})
+
+
+
+
+
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+
