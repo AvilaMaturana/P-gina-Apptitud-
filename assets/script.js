1fd0c593069765bd3d322fe857aec774764af85f
Diego Ávila Maturana <94457305+AvilaMaturana@users.noreply.github.com>
2022-03-01 21:35:30 -0300

    final

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
 
+   
+
   window.addEventListener('resize', function(){
     var nav = document.querySelector('nav');
     nav.classList.toggle('navbar-light' , window.innerWidth<1000);
 });  
 
-window.addEventListener('resize', function(){
-  var nav = document.getElementById('#navbarDos');
-  nav.classList.toggle('navbar-light' , window.innerWidth<1000);
-}); 
 
-})
 
+
+});
 
 
 
